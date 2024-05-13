b198b62c077630e1493d46a1dabf02600e2aee33
taiwo2 <adetonat@ymail.com>
2024-05-13 14:46:08 +0100

    Update views

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,25 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      
+      <Stack.Screen
+        name="[id]"
+        options={{ headerShown: true, headerTitle: 'Restaurant Details' }}
+      />
+      <Stack.Screen
+        name="modalFood"
+        options={{
+          presentation: 'modal',
+          headerTitle: '',
+          headerTransparent: true,
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ backgroundColor: '#fff', borderRadius: 20, padding: 2 }}
+              onPress={() => navigation.goBack()}>
+              <Ionicons name="close-outline" size={30} color={'#5a5a5a'} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 }
