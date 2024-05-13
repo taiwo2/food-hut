6e5cbeea85e67003975b653530e27d1330bd633d
taiwo2 <adetonat@ymail.com>
2024-05-13 14:33:05 +0100

    Add food home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,14 @@
-import { Entypo, Feather, Ionicons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, useNavigation } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
 
-export default function TabLayout() {
+export default function RootLayout() {
+  const navigation = useNavigation();
+
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: 'black',
-      }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: 'Home',
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: 'Search',
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="orders"
-        options={{
-          title: 'Orders',
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <Ionicons name="receipt-outline" size={24} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <Feather name="user" size={24} color={color} />,
-        }}
-      />
-    </Tabs>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,8 @@
+import { Redirect } from 'expo-router';
+import React from 'react';
 
-import { Link, Stack } from 'expo-router';
-import { View } from 'react-native';
-import { Text } from '~/components/nativewindui/Text';
+const MainScreen = () => {
+  return <Redirect href="/home" />;
+};
 
-export default function Screen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'index' }} />
-      <View className='flex-1 items-center justify-center p-5 bg-background'>
-        <Text variant='largeTitle'>This screen doesn'tgdgfjjfj existjhuigiu.</Text>
-
-        <Link href='/' className='m-4 py-4'>
-          <Text>Go to home screen!jjjjoooffgdj</Text>
-        </Link>
-      </View>
-    </>
-  );
-}
+export default MainScreen;
