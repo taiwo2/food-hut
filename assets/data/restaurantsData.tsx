80e7a5bdf32a17211d31917adf075965fca4d812
taiwo2 <adetonat@ymail.com>
2024-05-13 14:42:50 +0100

    Add detail route

--- assets/data/restaurantsData.tsx
+++ assets/data/restaurantsData.tsx
@@ -6,7 +6,7 @@ const dummyRestaurantsData = [
     price: 3,
     distance: '0.85',
     delivery: '10-20',
-    profileImage: 'https://x7d4c5z5.stackpathcdn.com/wp-content/uploads/tc/2021/03/og-img.jpg',
+    profileImage: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     about: 'Your go-to place for quick and tasty meals delivered right to your doorstep.',
     food: [
       {
--- components/marketCard.tsx
+++ components/marketCard.tsx
@@ -27,10 +27,10 @@ const MarketCard = ({ restaurantData }) => {
           <Text className={styles.restaurantName}>{restaurantData.name}</Text>
           <View className={styles.ratingContainer}>
             <FontAwesome name="star" size={17} color={ratingStyle.color} />
-            <Text className={styles.rating}>{restaurantData.rating}</Text>
+            <Text className={styles.rating}>£ {restaurantData.rating}</Text>
           </View>
         </View>
-        <Text className={styles.price}>{restaurantData.price} €</Text>
+        <Text className={styles.price}>{restaurantData.price}</Text>
       </Pressable>
     </Link>
   );
